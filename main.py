827de9b5917bd9cdf34bf2dcf5af32d966e93e44
Jordan Welsh <jack.boost@gmail.com>
2020-07-11 00:47:12 -0700

    STARTING IDEAS

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ Spawn in a mysterious text-based world with severe amnesia, guided by branching 
 rudimentary turn-based combat, inventory and status static meters/boxes
 gotta refresh the console every frame
 i'd love to be able to write the text onscreen letter by letter instead of a giant refresh
-turns out that YOU were the one who killed the princess after all! AUDIBLE GASP
+turns out that YOU were the one who killed the princess after all! AUDIBLE GASP!!!
 """
