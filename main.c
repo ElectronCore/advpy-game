e603e039972a5bbd161c47754675a74df6b651d0
ElectronCore <jack.boost@gmail.com>
2021-09-20 17:54:10 -0700

    change to C

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-"""
-Simple text-based branching adventure game
+
+/*Simple text-based branching adventure game
 using the console, maybe some ASCII, perhaps try a Qt integration at some point.
-Spawn in a mysterious text-based world with severe amnesia, guided by branching dialogue boxes. 
+Spawn in a mysterious text-based world with severe amnesia, guided by branching dialogue boxes.
 rudimentary turn-based combat, inventory and status static meters/boxes
 gotta refresh the console every frame
 i'd love to be able to write the text onscreen letter by letter instead of a giant refresh
 turns out that YOU were the one who killed the princess after all! AUDIBLE GASP!!
-"""
+*/
